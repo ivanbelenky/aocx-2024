ac8aa2c9243690e675fc63c0013ffc32c47c134c
ivanbelenky <ivandietert@gmail.com>
2024-12-09 12:25:44 -0300

    working 7.2

--- lib/bridge_repair.ex
+++ lib/bridge_repair.ex
@@ -15,6 +15,7 @@ defmodule BridgeRepair do
     case Enum.at(eq, idx) do
       "*" -> result * exec_equation(eq, idx-1)
       "+" -> result + exec_equation(eq, idx-1)
+      "||" -> String.to_integer(to_string(exec_equation(eq, idx-1)) <> to_string(result))
       :nil -> result
       x -> exec_equation(eq, idx-1, x)
     end
@@ -49,7 +50,8 @@ defmodule BridgeRepair do
     end)
   end
 
-  def eq(n, operands, ops) do
+  def eq(operands, ops) do
+    n = length(operands)*2 - 1
     Enum.map(0..n-1, fn idx ->
       case rem(idx, 2) do
         0 -> Enum.at(operands, div(idx, 2))
@@ -58,22 +60,41 @@ defmodule BridgeRepair do
     end)
   end
 
-  def possible2?(result, operands) do
-    # I need to split into all the possible 2 equations. That can be done by chossing
-    # all splits
-    n = length(operands)
-    all_splits = Enum.map(1..n-2, )
 
-    operators = all_operation_combinations(operands)
+  def all_operation_combinations_concat(operands) do
+    n = length(operands) - 1
+    Combinatorics.product(Enum.map(1..n, fn _ -> ["*", "+", "||"] end))
+  end
+
+
+  def possible2?(result, operands) do
+    operators = all_operation_combinations_concat(operands)
     Enum.any?(operators, fn ops ->
       n = length(operands)*2 - 1
-      eq = eq(n, operands, ops)
-
+      eq = Enum.map(0..n-1, fn idx ->
+        case rem(idx, 2) do
+          0 -> Enum.at(operands, div(idx, 2))
+          1 -> Enum.at(ops, div(idx, 2))
+        end
+      end)
       exec_equation(List.insert_at(eq, -1, "*"), length(eq)) == result
     end)
   end
+
 end
 
 
-# BridgeRepair.parse_input(File.read!("./lib/input/bridge_repair.txt"))
-# Enum.filter(resop, fn {res, ops} -> BridgeRepair.possible?(res, ops) end) |> Enum.reduce(0, fn {res, _}, acc -> acc+res end)
+# resop = BridgeRepair.parse_input(File.read!("./lib/input/bridge_repair.txt"))
+# resop = BridgeRepair.parse_input(ipt)
+# Enum.filter(resop, fn {res, ops} ->
+#  BridgeRepair.possible2?(res, ops)
+# end) |> Enum.reduce(0, fn {res, _}, acc -> acc+res end)
+# ipt = ~S"190: 10 19
+# 3267: 81 40 27
+# 83: 17 5
+# 156: 15 6
+# 7290: 6 8 6 15
+# 161011: 16 10 13
+# 192: 17 8 14
+# 21037: 9 7 18 13
+# 292: 11 6 16 20"
